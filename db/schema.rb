88340b5bf7a3c0cb55c51edbfc8096b378a78859
Alejandro Bringas <bringasalejandro@gmail.com>
2020-06-23 15:54:29 +0200

    Added status to report

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_110157) do
+ActiveRecord::Schema.define(version: 2020_06_23_134224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_06_23_110157) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.integer "status", default: 0, null: false
     t.index ["category_id"], name: "index_reports_on_category_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
