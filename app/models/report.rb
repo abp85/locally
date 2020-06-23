a0d0d243da8931ded6ac639547a672e6a2fc3a2d
Alejandro Bringas <bringasalejandro@gmail.com>
2020-06-23 15:29:46 +0200

    Models validations

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,11 @@ class Report < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :votes
+  has_one_attached :photo
+
+  validates :title, :description, :location, presence: true
+  validates :title, length: { maximun: 30 }
+
+  # geocoded_by :location
+  # after_validation :geocode, if: :will_save_change_to_location?
 end
