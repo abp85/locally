aeac302c9463e124e2a0365d069d08652fc17c59
Francesco Biedermann <francesco.biedermann@outlook.de>
2020-06-29 16:29:43 +0200

    modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,67 +35,67 @@ p "Nico's cat is creating Reports..."
 
 puts "1"
 file = URI.open("https://upload.wikimedia.org/wikipedia/commons/2/2b/Ickworth_Park%2C_Suffolk%2C_England_-broken_bench-2March2012.jpg")
-report = Report.create!(title:"Broken park bench", description: "This park bench in Carrer de Sant Pau has been broken for several months now and has to be replaced! It is very dangerous for elderly people and young children.", location: "Carrer de Sant Pau 44, Barcelona", user: User.all.sample, category: Category.all[0])
+report = Report.create!(title:"Broken park bench", description: "This park bench in Carrer de Sant Pau has been broken for several months now and has to be replaced! It is very dangerous for elderly people and young children.", location: "Carrer de Sant Pau 44, Barcelona", user: User.all.sample, category: Category.all[0], report_votes: 0)
 report.photo.attach(io: file, filename: "park-bench.jpg", content_type: 'image/jpg')
 puts "2"
 file = URI.open("https://images.theconversation.com/files/233448/original/file-20180824-149463-1hzm435.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop")
-report = Report.create!(title:"Dangerous pothole", description: "There is a dangerous pothole on Rambla del Raval that already lead to several accidents. It should be fixed as soon as possible!", location: "Rambla del Raval 4, Barcelona", user: User.all.sample, category: Category.all[3])
+report = Report.create!(title:"Dangerous pothole", description: "There is a dangerous pothole on Rambla del Raval that already lead to several accidents. It should be fixed as soon as possible!", location: "Rambla del Raval 4, Barcelona", user: User.all.sample, category: Category.all[3], report_votes: 0)
 report.photo.attach(io: file, filename: "pothole.jpg", content_type: 'image/jpg')
 puts "3"
 file = URI.open("https://www.dailynews.com/wp-content/uploads/2018/06/TRASH-HAUL-street_trash_2_061618.jpg")
-report = Report.create!(title:"Piles of trash", description: "The trash cans are always full and never get emptied. It attracts rats and other animals and smells horrible! This has to change.", location: "Carrer de l'Hospital 46, Barcelona", user: User.all.sample, category: Category.all[8])
+report = Report.create!(title:"Piles of trash", description: "The trash cans are always full and never get emptied. It attracts rats and other animals and smells horrible! This has to change.", location: "Carrer de l'Hospital 46, Barcelona", user: User.all.sample, category: Category.all[8], report_votes: 0)
 report.photo.attach(io: file, filename: "trash.jpg", content_type: 'image/jpg')
 puts "4"
 file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Street_lamp%2C_Edinburgh_New_Town.jpg/1200px-Street_lamp%2C_Edinburgh_New_Town.jpg")
-report = Report.create!(title:"Burnt out Streetlight", description: "One of the street lights in Carrer de l'Arc del Teatre is burnt out and should be replaced. It is a dark alley and especially during the night it is dangerous.", location: "Carrer de l'Arc del Teatre 9, Barcelona", user: User.all.sample, category: Category.all[2])
+report = Report.create!(title:"Burnt out Streetlight", description: "One of the street lights in Carrer de l'Arc del Teatre is burnt out and should be replaced. It is a dark alley and especially during the night it is dangerous.", location: "Carrer de l'Arc del Teatre 9, Barcelona", user: User.all.sample, category: Category.all[2], report_votes: 0)
 report.photo.attach(io: file, filename: "streetlight.jpg", content_type: 'image/jpg')
 puts "5"
 file = URI.open("https://upload.wikimedia.org/wikipedia/commons/7/75/Great_Portland_Street_London_Traffic.jpg")
-report = Report.create!(title:"Noisy traffic", description: "The expansion of Carrer de la Riera Alta has lead to serious noise issues for residents. We can't sleep at night anymore and the pollution is horrible.", location: "Carrer de la Riera Alta 23, Barcelona", user: User.all.sample, category: Category.all[9])
+report = Report.create!(title:"Noisy traffic", description: "The expansion of Carrer de la Riera Alta has lead to serious noise issues for residents. We can't sleep at night anymore and the pollution is horrible.", location: "Carrer de la Riera Alta 23, Barcelona", user: User.all.sample, category: Category.all[9], report_votes: 0)
 report.photo.attach(io: file, filename: "traffic.jpg", content_type: 'image/jpg')
 puts "6"
 file = URI.open("https://arc-anglerfish-arc2-prod-tbt.s3.amazonaws.com/public/4ABN7POFTFHENBPKDI4QYDLBQY.jpg")
-report = Report.create!(title:"Sewage leak", description: "There has been a sewage leak on Carrer de Jaume I. It needs to be fixed immediately, the smell is disgusting and drives out the tourists.", location: "Carrer de Jaume I 14, Barcelona", user: User.all.sample, category: Category.all[1])
+report = Report.create!(title:"Sewage leak", description: "There has been a sewage leak on Carrer de Jaume I. It needs to be fixed immediately, the smell is disgusting and drives out the tourists.", location: "Carrer de Jaume I 14, Barcelona", user: User.all.sample, category: Category.all[1], report_votes: 0)
 report.photo.attach(io: file, filename: "sewage.jpg", content_type: 'image/jpg')
 puts "7"
 file = URI.open("https://3kpnuxym9k04c8ilz2quku1czd-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/42615859735_d3100ec492_o.jpg")
-report = Report.create!(title:"Plants need watering", description: "The entire flower bed on Carrer Nou de Sant Francesc  needs watering! The plants are dying out due to the immense heat.", location: "Carrer Nou de Sant Francesc 1, Barcelona", user: User.all.sample, category: Category.all[0])
+report = Report.create!(title:"Plants need watering", description: "The entire flower bed on Carrer Nou de Sant Francesc  needs watering! The plants are dying out due to the immense heat.", location: "Carrer Nou de Sant Francesc 1, Barcelona", user: User.all.sample, category: Category.all[0], report_votes: 0)
 report.photo.attach(io: file, filename: "plants.jpg", content_type: 'image/jpg')
 puts "8"
 file = URI.open("https://www.clubbookers.com/wp-content/uploads/2017/05/Pacha-Barcelona.jpg")
-report = Report.create!(title:"Noisy night club", description: "The opening of the Pacha night club has brought disturbing noise to the neighbourhood and it's impossible to get rest at the weekend. It should not be permitted in a residential area!", location: "Carrer dels Còdols 12, Barcelona", user: User.all.sample, category: Category.all[9])
+report = Report.create!(title:"Noisy night club", description: "The opening of the Pacha night club has brought disturbing noise to the neighbourhood and it's impossible to get rest at the weekend. It should not be permitted in a residential area!", location: "Carrer dels Còdols 12, Barcelona", user: User.all.sample, category: Category.all[9], report_votes: 0)
 report.photo.attach(io: file, filename: "club.jpg", content_type: 'image/jpg')
 puts "9"
 file = URI.open("https://static.timesofisrael.com/www/uploads/2018/11/iStock-938520600-e1542280189799.jpg")
-report = Report.create!(title:"Smelly dog poop", description: "People do not collect the poop of their dogs in our neighbourhood and the street is full of dog turds. There city should hand out free plastic bags around the city in order to prevent this.", location: "Plaça de les Olles, Barcelona", user: User.all.sample, category: Category.all[8])
+report = Report.create!(title:"Smelly dog poop", description: "People do not collect the poop of their dogs in our neighbourhood and the street is full of dog turds. There city should hand out free plastic bags around the city in order to prevent this.", location: "Plaça de les Olles, Barcelona", user: User.all.sample, category: Category.all[8], report_votes: 0)
 report.photo.attach(io: file, filename: "dog-poop.jpg", content_type: 'image/jpg')
 puts "10"
 file = URI.open("https://www.swarco.com/sites/default/files/public/hero/image/2018-10/cyclingpath_header.jpg")
-report = Report.create!(title:"Bicycle path", description: "We need a bicycle path in our neighbourhood! The children are forced to drive in the heavy traffic making it extremely dangerous!", location: "Passeig de Picasso 40, Barcelona", user: User.all.sample, category: Category.all[6])
+report = Report.create!(title:"Bicycle path", description: "We need a bicycle path in our neighbourhood! The children are forced to drive in the heavy traffic making it extremely dangerous!", location: "Passeig de Picasso 40, Barcelona", user: User.all.sample, category: Category.all[6], report_votes: 0)
 report.photo.attach(io: file, filename: "bicycle.jpg", content_type: 'image/jpg')
 puts "11"
 file = URI.open("https://live.staticflickr.com/4099/4774162801_ffb7d48383_b.jpg")
-report = Report.create!(title:"Parking situation", description: "The parking situation on Carrer de Verdi is terrible. It is close to impossible to find a spot to park. The city should construct more parking spaces!", location: "Carrer de Verdi 2, Barcelona", user: User.all.sample, category: Category.all[6])
+report = Report.create!(title:"Parking situation", description: "The parking situation on Carrer de Verdi is terrible. It is close to impossible to find a spot to park. The city should construct more parking spaces!", location: "Carrer de Verdi 2, Barcelona", user: User.all.sample, category: Category.all[6], report_votes: 0)
 report.photo.attach(io: file, filename: "parking.jpg", content_type: 'image/jpg')
 puts "12"
 file = URI.open("https://www.powermag.com/wp-content/uploads/2020/06/uniper-kraftwerk-datteln.jpg")
-report = Report.create!(title:"Power plant", description: "We are agains the construction of the Nuclear Power Plant on Carrer d'Astúries. It would make the neighbourhood unhabitable and we would be forced to move. Not mentioning the toxic radiation and noise that comes with it. Stop it now!!", location: "Carrer d'Astúries 4, Barcelona", user: User.all.sample, category: Category.all[3])
+report = Report.create!(title:"Power plant", description: "We are agains the construction of the Nuclear Power Plant on Carrer d'Astúries. It would make the neighbourhood unhabitable and we would be forced to move. Not mentioning the toxic radiation and noise that comes with it. Stop it now!!", location: "Carrer d'Astúries 4, Barcelona", user: User.all.sample, category: Category.all[3], report_votes: 0)
 report.photo.attach(io: file, filename: "power-plant.jpg", content_type: 'image/jpg')
 puts "13"
 file = URI.open("https://s7d2.scene7.com/is/image/TWCNews/12-11-2019_Daytona_Beach_Homeless_Shelter")
-report = Report.create!(title:"Homeless shelter", description: "Authorities should build a homeless shelter in Gracia in order to help the people in need. There are many people living on the streets and they deserve to have a home.", location: "Carrer de Maria 7, Barcelona", user: User.all.sample, category: Category.all[0])
+report = Report.create!(title:"Homeless shelter", description: "Authorities should build a homeless shelter in Gracia in order to help the people in need. There are many people living on the streets and they deserve to have a home.", location: "Carrer de Maria 7, Barcelona", user: User.all.sample, category: Category.all[0], report_votes: 0)
 report.photo.attach(io: file, filename: "homeless.jpg", content_type: 'image/jpg')
 puts "14"
 file = URI.open("https://img.blogs.es/smartcitylab/wp-content/uploads/2019/07/vienna-smart-traffic-lights.jpg")
-report = Report.create!(title:"Defect traffic light", description: "There is a defect traffic light on Carrer dels Almogàvers. It should be fixed asap in order to make the traffic safer.", location: "Carrer dels Almogàvers 122, Barcelona", user: User.all.sample, category: Category.all[3])
+report = Report.create!(title:"Defect traffic light", description: "There is a defect traffic light on Carrer dels Almogàvers. It should be fixed asap in order to make the traffic safer.", location: "Carrer dels Almogàvers 122, Barcelona", user: User.all.sample, category: Category.all[3], report_votes: 0)
 report.photo.attach(io: file, filename: "traffic-light.jpg", content_type: 'image/jpg')
 puts "15"
 file = URI.open("https://i.pinimg.com/originals/77/3c/db/773cdbc54faad54f1954648a73e921cd.jpg")
-report = Report.create!(title:"Soccer pitch", description: "It would be great to have a public soccer pitch in Poblenou. There is free space in the park which could be used.", location: "Carrer de Zamora 78, Barcelona", user: User.all.sample, category: Category.all[0])
+report = Report.create!(title:"Soccer pitch", description: "It would be great to have a public soccer pitch in Poblenou. There is free space in the park which could be used.", location: "Carrer de Zamora 78, Barcelona", user: User.all.sample, category: Category.all[0], report_votes: 0)
 report.photo.attach(io: file, filename: "soccer.jpg", content_type: 'image/jpg')
 puts "16"
 file = URI.open("https://i.pinimg.com/originals/a1/48/9d/a1489dffc6594d0014a692ec0ef99006.jpg")
-report = Report.create!(title:"Missing elevator", description: "There is no elevator in the Metro station Rambla del Poblenou. It is impossible for disabled and elderly people to use the stairs and therefore an elevator should be built!", location: "Rambla del Poblenou, 122, Barcelona", user: User.all.sample, category: Category.all[3])
+report = Report.create!(title:"Missing elevator", description: "There is no elevator in the Metro station Rambla del Poblenou. It is impossible for disabled and elderly people to use the stairs and therefore an elevator should be built!", location: "Rambla del Poblenou, 122, Barcelona", user: User.all.sample, category: Category.all[3], report_votes: 0)
 report.photo.attach(io: file, filename: "elevator.jpg", content_type: 'image/jpg')
 
 p "Nico's cat is creating Votes..."
