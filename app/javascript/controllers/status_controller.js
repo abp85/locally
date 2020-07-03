ef6858a8132181cffe5a7ca13297ebfb3b133d16
Alejandro Bringas <bringasalejandro@gmail.com>
2020-07-03 10:30:45 +0200

    Fixed some bug in profile

--- app/javascript/controllers/status_controller.js
+++ app/javascript/controllers/status_controller.js
@@ -5,11 +5,11 @@ export default class extends Controller {
 
   resolve(event) {
     const target = event.target
-    console.log(target)
+    // console.log(target)
     fetch(`/reports/${target.dataset.reportId}/resolve`, { headers: { accept: "application/json" } })
         .then(response => response.json())
         .then((data) => {
-          console.log(data)
+          // console.log(data)
         const button = document.getElementById(`resolve-${target.dataset.reportId}`);
         const card = document.getElementById(`report-${target.dataset.reportId}`);
         const pill = document.getElementById(`changestatus-${target.dataset.reportId}`);
@@ -17,39 +17,39 @@ export default class extends Controller {
         const solvedCont = document.getElementById(`solved`);
         const openReports = document.getElementById('open-reports');
         const solvedReports = document.getElementById('solved-reports');
-        let cardNotif
+        // let cardNotif
         if (data.status === 'resolved'){
-          cardNotif = document.getElementById(`card-notification-open`);
+          // cardNotif = document.getElementById(`card-notification-open`);
           // button.className = 'fas fa-check-square';
-          cardNotif.innerHTML = ""
-          cardNotif.style.opacity = 1
-          cardNotif.className = "pill-category"
+          // cardNotif.innerHTML = ""
+          // cardNotif.style.opacity = 1
+          // cardNotif.className = "pill-category"
           openedCont.removeChild(card);
           solvedCont.insertAdjacentElement(`afterbegin`, card)
           button.innerHTML = "Mark as open"
           pill.className = "pill-status-solved"
           pill.innerHTML = "solved"
-          cardNotif.innerHTML = "Moved to Solved"
+          // cardNotif.innerHTML = "Moved to Solved"
         } else {
           // button.className = 'far fa-check-square';
-          cardNotif = document.getElementById(`card-notification-solved`);
-          cardNotif.innerHTML = ""
-          cardNotif.style.opacity = 1
-          cardNotif.className = "pill-category"
+          // cardNotif = document.getElementById(`card-notification-solved`);
+          // cardNotif.innerHTML = ""
+          // cardNotif.style.opacity = 1
+          // cardNotif.className = "pill-category"
           solvedCont.removeChild(card);
           openedCont.insertAdjacentElement(`afterbegin`, card)
           button.innerHTML = "Mark as solved"
           pill.className = "pill-status-open"
           pill.innerHTML = "pending"
-          cardNotif.innerHTML = "Moved to Open"
+          // cardNotif.innerHTML = "Moved to Open"
         }
 
         openReports.innerHTML = data.user_open_reports
         solvedReports.innerHTML = data.user_solved_reports
 
-        setTimeout(() => {
-          cardNotif.style.opacity = 0
-        }, 4000)
+        // setTimeout(() => {
+          // cardNotif.style.opacity = 0
+        // }, 4000)
         // setTimeout(() => {
         // }, 5000)
       });
